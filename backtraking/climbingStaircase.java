9ae44c27961e946ba3224162587376dbac686495
Pranay <30526546+pranaynagpure321@users.noreply.github.com>
2022-02-09 11:44:02 +0530

    Update climbingStaircase.java

--- backtraking/climbingStaircase.java
+++ backtraking/climbingStaircase.java
@@ -1,4 +1,18 @@
+/* discription
+You need to climb a staircase that has n steps, and you decide to get some extra exercise by jumping up the steps. 
+You can cover at most k steps in a single jump. Return all the possible sequences of jumps that you could take to climb the staircase, sorted.
 
+Example
+
+For n = 4 and k = 2, the output should be
+
+solution(n, k) =
+[[1, 1, 1, 1],
+ [1, 1, 2],
+ [1, 2, 1],
+ [2, 1, 1],
+ [2, 2]]
+ */
 
 public static  int sum =0;
 public static LinkedList<Integer> ll = new LinkedList<>();
