13d94cff352b0f2b96b97974a01fb9fbfe8c192d
Pranay <30526546+pranaynagpure321@users.noreply.github.com>
2022-03-24 19:16:11 +0530

    Update ninja_at_gym.java

--- dynamic_programing/coding_ninja/ninja_at_gym.java
+++ dynamic_programing/coding_ninja/ninja_at_gym.java
@@ -2,22 +2,26 @@ import java.util.ArrayList;
 
 public class Solution {
     static int max=0;
-    int[] dp;
-    public static void dfs(ArrayList<Integer> rack,int n,int start,int previous,int sum)
+    static int[] dp;
+    public static int  dfs(ArrayList<Integer> rack,int n,int start,int previous,int sum)
     {
-         if(sum>max)
-            max=sum;
-        if(dp[start]!=0)
-            return ;
+        
+        if(dp[start] != 0)
+            return dp[start];
+        
         for(int i = start+1 ;i < n ;i++)
         {
             if(previous<rack.get(i)){
-                sum +=rack.get(i);
-                dfs(rack,n,i,rack.get(i),sum);
-                sum -=rack.get(i);
+             
+              int a =  dfs(rack,n,i,rack.get(i),sum);
+                if(a>dp[start])
+                    dp[start]=a;
+               
             }
         }
-            
+        
+        dp[start] +=rack.get(start);
+        return dp[start] ;
     }
     public static int maxIncreasingDumbbellsSum(ArrayList<Integer> rack, int n) {
         // Write your code here.
@@ -28,7 +32,13 @@ public class Solution {
         dp = new int[n];
         for(int i =0 ;i< n ;i++){
          
-            dfs(rack,n, i,rack.get(i),rack.get(i));
+           dfs(rack,n, i,rack.get(i),rack.get(i));
+        }
+        
+        for(int a: dp)
+        {
+            if(a>max)
+                max=a;
         }
         
         return max;
