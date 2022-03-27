dc5e47da0ee1687c9ea2b15c41874031741f1585
Pranay <30526546+pranaynagpure321@users.noreply.github.com>
2022-03-27 13:40:46 +0530

    Create maximum_subarray_kadane.java

---
+++ dynamic_programing/coding_ninja/maximum_subarray_kadane.java
@@ -0,0 +1,20 @@
+public class Solution {
+ 
+   
+    public static long maxSubarraySum(int[] arr, int n) {
+        // write your code here
+      int max=0;
+      int curr_sum=0;
+      for(int i =0;i<n;i++)
+      {
+            curr_sum+= arr[i];
+            if(curr_sum>max)
+                max=curr_sum;
+          
+            if(curr_sum<0)
+                curr_sum=0;
+      }     
+        return max;
+    }
+ 
+}
