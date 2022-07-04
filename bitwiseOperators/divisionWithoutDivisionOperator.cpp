20b798eec4f599c9b8aef79b3f907fbac05df41d
Pranay <30526546+pranaynagpure321@users.noreply.github.com>
2022-07-04 22:43:26 +0530

    two different approaches for division without division

--- bitwiseOperators/divisionWithoutDivisionOperator.cpp
+++ bitwiseOperators/divisionWithoutDivisionOperator.cpp
@@ -1,3 +1,5 @@
+int Solution::divide(int A, int B) {
+
 if( B==0) return  INT_MAX;
     if(B== 1) return A;
     if(B==-1 && A>0) return B*A; 
@@ -16,3 +18,33 @@ if( B==0) return  INT_MAX;
         q++;
     }
     return q*sign;
+
+
+}
+
+int Solution::divide(int A, int B) {
+
+    if( B==0) return  INT_MAX;
+    if(B== 1) return A;
+    if(B==-1 && A>0) return B*A; 
+    if(B==-1 && A==INT_MIN) return INT_MAX;
+    int  sign = ((A<0) ^ (B<0)) ?-1:1;
+ 
+    
+   long long dividend = abs((long long)A);
+   long long divisor = abs((long long)B);
+   int quo = 0,sum=0;
+
+   for (int i = 31; i >=0 ; i--)
+   {
+    
+       if((sum + (divisor << i)) <= dividend )
+       {
+            sum += divisor<<i;
+            quo += 1<<i;
+       }
+      
+   }
+   
+   return sign*quo;
+}
