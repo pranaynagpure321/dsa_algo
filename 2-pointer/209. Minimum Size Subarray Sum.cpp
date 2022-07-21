ef63618d376bc158ef6252bbf9836dfea33635b7
Pranay <30526546+pranaynagpure321@users.noreply.github.com>
2022-07-21 18:32:59 +0530

    O(N) approch to minsubarraysum problem

--- 2-pointer/209. Minimum Size Subarray Sum.cpp
+++ 2-pointer/209. Minimum Size Subarray Sum.cpp
@@ -33,3 +33,30 @@ public:
         return ans+1;
     }
 };
+
+
+
+
+//#another solution with more understtandable way O(N)
+
+int  minSubArrayLen_3(int target, vector<int>& nums)
+{
+    int left =0 , j =0;
+
+    int sum =0; int ans = INT_MAX;
+    
+    for( ; j< nums.size() ; j++)
+    {
+       sum += nums[j];
+
+       while (sum >= target)
+       {
+          ans = min(ans, j+1-i);
+          sum -= nums[left++];
+       }
+       
+    }
+
+  return ans == INT_MAX ? 0: ans;
+
+}
