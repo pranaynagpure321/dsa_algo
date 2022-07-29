57a497966880efd7b0b893d577e03154e41574ce
Pranay <30526546+pranaynagpure321@users.noreply.github.com>
2022-07-29 16:32:30 +0530

    Update jump_game.java

--- dynamic_programing/leetcode/jump_game.java
+++ dynamic_programing/leetcode/jump_game.java
@@ -1,19 +1,10 @@
 /*
 55. Jump Game
 Medium
-
-12191
-
-661
-
-Add to List
-
-Share
+ 
 You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
 
 Return true if you can reach the last index, or false otherwise.
-
- 
 
 Example 1:
 
