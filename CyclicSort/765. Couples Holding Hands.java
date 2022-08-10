f057f952c1ef41febb26e179c1ccd20f4b4ba6c3
Pranay <30526546+pranaynagpure321@users.noreply.github.com>
2022-08-10 18:38:30 +0530

    Update 765. Couples Holding Hands.java

--- CyclicSort/765. Couples Holding Hands.java
+++ CyclicSort/765. Couples Holding Hands.java
@@ -23,6 +23,13 @@ Output: 1
 Explanation: We only need to swap the second (row[1]) and third (row[2]) person.
 Example 2:
 
+[0,2,1,3]
+[3,2,0,1]
+[2,0,5,4,3,1]
+[0,2,4,6,7,1,3,5]
+[1,4,0,5,8,7,6,3,2,9]
+[5,4,2,6,3,1,0,7]
+
 */
 
 
