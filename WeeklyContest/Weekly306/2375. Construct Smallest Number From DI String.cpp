248c260835a7bc7a339ce0acb1b78901926f59a7
Pranay <30526546+pranaynagpure321@users.noreply.github.com>
2022-08-14 22:50:06 +0530

    Update 2375. Construct Smallest Number From DI String.cpp

--- WeeklyContest/Weekly306/2375. Construct Smallest Number From DI String.cpp
+++ WeeklyContest/Weekly306/2375. Construct Smallest Number From DI String.cpp
@@ -13,6 +13,21 @@ If pattern[i] == 'I', then num[i] < num[i + 1].
 If pattern[i] == 'D', then num[i] > num[i + 1].
 Return the lexicographically smallest possible string num that meets the conditions.
 
+
+string smallestNumber(string pattern) {
+        stack<int> s;
+        string ans = "";
+        if(pattern[0] == 'I') ans += '1';
+        else s.push('1');
+        for(int i = 1; i <= pattern.size(); i++){
+            if(pattern[i-1] == 'I'){
+                while(!s.empty()){  ans += s.top(); s.pop(); }
+            }
+            s.push('0' + i+1);
+        }
+        while(!s.empty()){ ans += s.top(); s.pop();  }
+        return ans;
+    }
  */
 
 typedef long long ll;
