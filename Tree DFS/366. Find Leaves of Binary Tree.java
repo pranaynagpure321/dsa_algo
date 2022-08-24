0328851922fcc96b1f39e11839c91c98dfdeb318
Pranay <30526546+pranaynagpure321@users.noreply.github.com>
2022-08-24 19:43:48 +0530

    reverse traversal

--- Tree DFS/366. Find Leaves of Binary Tree.java
+++ Tree DFS/366. Find Leaves of Binary Tree.java
@@ -1,4 +1,28 @@
 /**
+366. Find Leaves of Binary Tree
+Medium
+
+ 
+
+Add to List
+
+Share
+Given the root of a binary tree, collect a tree's nodes as if you were doing this:
+
+Collect all the leaf nodes.
+Remove all the leaf nodes.
+Repeat until the tree is empty.
+ 
+Example 1:
+
+Input: root = [1,2,3,4,5]
+Output: [[4,5,3],[2],[1]]
+Explanation:
+[[3,5,4],[2],[1]] and [[3,4,5],[2],[1]] are also considered correct answers since per each level it does not matter the order on which elements are returned.
+Example 2:
+
+Input: root = [1]
+Output: [[1]]
  * Definition for a binary tree node.
  * public class TreeNode {
  *     int val;
