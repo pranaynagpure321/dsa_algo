6d07dd9be088b49dfe8da3b13ea64ddda80245f3
Pranay <30526546+pranaynagpure321@users.noreply.github.com>
2022-09-24 22:46:46 +0530

    check if all the bits exists in given string

---
+++ bitwiseOperators/1461. Check If a String Contains All Binary Codes of Size K.cpp
@@ -0,0 +1,61 @@
+/*1461. Check If a String Contains All Binary Codes of Size K
+Medium
+
+1928
+
+90
+
+Add to List
+
+Share
+Given a binary string s and an integer k, return true if every binary code of length k is a substring of s. Otherwise, return false.
+
+ 
+
+Example 1:
+
+Input: s = "00110110", k = 2
+Output: true
+Explanation: The binary codes of length 2 are "00", "01", "10" and "11". They can be all found as substrings at indices 0, 1, 3 and 2 respectively.
+Example 2:
+
+Input: s = "0110", k = 1
+Output: true
+Explanation: The binary codes of length 1 are "0" and "1", it is clear that both exist as a substring. 
+Example 3:
+
+Input: s = "0110", k = 2
+Output: false
+Explanation: The binary code "00" is of length 2 and does not exist in the array.*/
+
+class Solution {
+public:
+    bool hasAllCodes(string s, int k) {
+         map<int ,int> mp;
+        int n = s.length();
+
+        for (int i = 0; i <n-k+1; i++) {
+
+            if(i+k <=n )
+            {
+                string tmp = s.substr(i,k);
+                int t = stoi(tmp, nullptr,2);
+                mp[t]=i;
+                
+                //cout<< "binary=> " << tmp << " location => " << i << " dec=> "<<t<<endl;
+            }
+
+        }
+        
+        int size = round(pow(2,k));
+        
+        for (int i = 0; i < size; i++) {
+            
+            if(mp.find(i)== mp.end()) return false;
+        }
+        
+        
+        // time O(n*k)
+        return true;
+    }
+};
