5aefe09954754ef2d16044bc4fb5c52595c2d0d1
Pranay <30526546+pranaynagpure321@users.noreply.github.com>
2022-11-05 00:46:28 +0530

    recursive optimized

--- dynamic_programing/leetcode/329. Longest Increasing Path in a Matrix.cpp
+++ dynamic_programing/leetcode/329. Longest Increasing Path in a Matrix.cpp
@@ -50,3 +50,43 @@ public:
         return ans+1;
     }
 };
+
+
+// more understandable approach
+class Solution {
+public:
+vector<vector<int>> dirs = {{0,1},{1,0},{-1,0},{0,-1}};
+int m,n;
+    int dfs(vector<vector<int>>& matrix , int i ,int j, vector<vector<int>> &dp )
+    {
+        if(dp[i][j]!=0) return dp[i][j];
+         //dfs in 4 direction
+        for(auto &v: dirs)
+        {
+            int x = i + v[0], y = j + v[1];
+            if (0 <= x && x < m && 0 <= y && y < n && matrix[x][y] > matrix[i][j])
+            dp[i][j]  = max(dp[i][j], dfs(matrix,x,y,dp));
+        }
+        return ++dp[i][j];
+
+    }
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        
+        m= matrix.size(); n= matrix[0].size();
+        vector<vector<bool>> vis( m, vector<bool>(n ,false) );
+        vector<vector<int>> dp(m, vector<int>(n,0));
+        int ans =0;
+        
+        for(int i =0; i<m; i++ )
+        {   
+            int t =0;
+            for(int j =0; j< matrix[i].size(); j++)
+            {
+                t = dfs(matrix, i,j,dp);
+                ans = max(t,ans);
+            } 
+        }
+        //o(mn)
+        return ans;
+    }
+};
