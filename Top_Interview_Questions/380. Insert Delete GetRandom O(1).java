9744bf429221d7d8f4c53e3bf93b367a70b5b81c
Pranay <30526546+pranaynagpure321@users.noreply.github.com>
2022-11-05 18:13:47 +0530

    must visit question for interview

---
+++ Top_Interview_Questions/380. Insert Delete GetRandom O(1).java
@@ -0,0 +1,56 @@
+class RandomizedSet {
+
+    ArrayList<Integer> arr   ;
+    HashMap<Integer,Integer> mp ;
+    Random random = new Random();   
+    public RandomizedSet() {
+        mp = new HashMap<Integer, Integer>();
+        arr  = new ArrayList<Integer>();
+    }
+    
+    public boolean insert(int val) {
+        boolean present = mp.containsKey(val);
+
+        if(present) return false;
+      
+        arr.add(val);
+        mp.put(val, arr.size()-1);  
+        return true;
+    }
+    
+    public boolean remove(int val) {
+
+        if(!mp.containsKey(val)) return false;
+        if(arr.size()==1 ) {arr.remove(0); mp.remove(val); return true;}
+
+        //swap last element with element we want to remove
+        // then remove last element from array as well as map
+        int last = arr.get(arr.size()-1);
+        int idx = mp.get(val);
+       // System.out.println("before removal=> "+Arrays.toString(arr.toArray()));
+        arr.set(idx,last);
+        arr.remove(arr.size()-1);
+
+        //System.out.println("After removal=> "+Arrays.toString(arr.toArray()));
+        mp.remove(val);
+        mp.replace(last,idx);
+        // for (var name: mp.keySet()) {
+        //     String key = name.toString();
+        //     String value = mp.get(name).toString();
+        //     System.out.println(key + " " + value);
+        // }
+        return true;
+    }
+    
+    public int getRandom() {
+        return arr.get(random.nextInt(arr.size()));
+    }
+}
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
