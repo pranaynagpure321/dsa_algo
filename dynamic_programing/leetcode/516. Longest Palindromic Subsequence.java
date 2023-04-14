a721274e625f22320c835668cd07d6a903d326f4
Pranay <30526546+pranaynagpure321@users.noreply.github.com>
2023-04-14 11:17:20 +0530

    recursive and tabulation

--- dynamic_programing/leetcode/516. Longest Palindromic Subsequence.java
+++ dynamic_programing/leetcode/516. Longest Palindromic Subsequence.java
@@ -26,3 +26,39 @@ class Solution {
         return help(s,0,s.length()-1, memo);
     }
 }
+
+
+
+//tabulation using lcs 
+
+class Solution {
+public:
+
+    int lcs(string s1 , string s2)
+    {
+        int n = s1.size();
+        vector<vector<int>> dp(n+1, vector<int>(n+1,0));
+
+        for(int i =1 ; i <= n ;i++)
+        {
+            for(int j = 1 ;j <= n ;j++)
+            {
+                if(s1[i-1] == s2 [j-1])
+                dp[i][j] = dp[i-1][j-1] +1;
+                else
+                {
+                    dp[i][j] = max(dp[i-1][j] , dp[i][j-1]);
+                }
+            }
+        }
+
+        return dp[n][n];
+    }
+    int longestPalindromeSubseq(string s) {
+
+        string s1 =s;
+        reverse(s.begin(), s.end());
+
+        return lcs(s1,s);
+    }
+};
