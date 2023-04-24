7062051b757eb590c77f792f1ca80430ea6c1308
Pranay <30526546+pranaynagpure321@users.noreply.github.com>
2023-04-24 16:37:17 +0530

    Create 1046. Last Stone Weight.cpp

---
+++ Heaps/1046. Last Stone Weight.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        
+        priority_queue<int > pq(stones.begin(), stones.end());
+        while(pq.size()>1)
+        {
+            int x = pq.top(); pq.pop();
+            int y = pq.top(); pq.pop();
+
+            pq.push(abs(x-y));
+        }
+
+        return pq.size()>0? pq.top() : 0;
+    }
+};
