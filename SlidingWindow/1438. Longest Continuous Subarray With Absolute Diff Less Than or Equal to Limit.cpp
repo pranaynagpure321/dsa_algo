c91dea9a764b99d7fb6814666cafc810bc808ddd
Pranay <30526546+pranaynagpure321@users.noreply.github.com>
2023-05-22 19:12:22 +0530

    Update 1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit.cpp
    multiset and de base problem

--- SlidingWindow/1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit.cpp
+++ SlidingWindow/1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit.cpp
@@ -45,3 +45,59 @@ class Solution {
         return j - i;
     }
 }
+
+
+// solution t based on deque
+class Solution {
+public:
+
+// use multiset to maintain the sorted order and check the first and last element diff to increase or decrese the window
+    // int longestSubarray(vector<int>& nums, int limit) {
+        
+    //     multiset<int> set;
+    //     int i=0 ,j=0;
+    //     for( j =0 ;j< nums.size(); j++)
+    //     {
+    //         set.insert(nums[j]);
+
+    //         if(*set.rbegin() - *set.begin() > limit)
+    //         set.erase(set.find(nums[i++]));
+    //     }
+
+    //     return j - i;
+    // }
+
+    int longestSubarray(vector<int>& nums, int limit) {
+    
+    deque<int> maxQ, minQ;
+
+    int i =0 , j,res =0 ;
+
+        for(j =0 ; j<nums.size() ; ++j)
+        {
+            while(!maxQ.empty() and maxQ.back() < nums[j] )
+            {
+                maxQ.pop_back();
+            }
+            maxQ.push_back(nums[j]);
+
+            while(!minQ.empty() and minQ.back() > nums[j])
+            {
+                minQ.pop_back();
+            }
+
+            minQ.push_back(nums[j]);
+
+            while(maxQ.front() - minQ.front() > limit)
+            {
+                if(maxQ.front() == nums[i]) maxQ.pop_front();
+                if(minQ.front() == nums[i]) minQ.pop_front();
+                i++;
+            }
+
+            res = max(res, j - i+1);
+        }
+
+        return res;
+    }
+};
