f05f4a4d026b3cfca59de8d82919f4fdea2e3652
Pranay <30526546+pranaynagpure321@users.noreply.github.com>
2023-06-27 17:37:37 +0530

    Update 120. Triangle.cpp
    bottom up approach added

--- dynamic_programing/leetcode/120. Triangle.cpp
+++ dynamic_programing/leetcode/120. Triangle.cpp
@@ -14,4 +14,31 @@ public:
         dp = vector<vector<int>>(n ,vector<int>(n,-1));
         return triangle[0][0] + dfs(triangle,1,0);
     }
+
+
+  // by flipping the traingle anf keeping track of only single row insted 
+  int minimumTotal(vector<vector<int>>& triangle) {
+  
+    int n = triangle.size();
+
+    vector<int> next(n,0);
+
+    for(int i =0 ; i< n ; ++i) next[i] = triangle[n-1][i];
+
+    for(int i = n-2  ; i>=0 ; --i)
+    {
+        vector<int> curr(n,0);
+
+        for(int j =0 ;j<= i ; j++)
+        {
+            int idx = triangle[i][j] + next[j];
+            int idx2 = triangle[i][j] + next[j+1];
+            curr[j]= min(idx,idx2);
+        }
+        next = curr;
+    }
+    return next[0];
+
+    //timeO(n2) space O(N)
+  }
 };
